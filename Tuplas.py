24c59d292639be2970356a2e42d6223a0d83d172
LRuizNoguera <johannruiz18@gmail.com>
2024-09-02 07:00:55 -0500

    Primer repositorio

---
+++ Tuplas.py
@@ -0,0 +1,7 @@
+"""" Tuplas son estructuras que permiten almacenar diferentes tipo de datos. Son inmutables
+por lo cual no permiten modificar o agregar elementos una vez se haya definido la tupla"""
+
+tupla = (1, "hola", 3.5)
+print(tupla)
+print(tupla[0])
+tupla[0] = 2
